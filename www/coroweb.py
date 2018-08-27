876d22080022121f9e6fd6de5a49021542ec343b
wbjxxzx <wbjxxzx@163.com>
2018-08-27 13:47:25 +0800

    finished web frame

--- www/coroweb.py
+++ www/coroweb.py
@@ -39,14 +39,14 @@ def post(path):
         return wrapper
     return decorator
 
-def get_required_kw_argss(fn):
+def get_required_kw_args(fn):
     args = []
     params = inspect.signature(fn).parameters
     for name, param in params.items():
-        if param.kind == inspect.Parameter.KEYWORD_ONLY
-            and param.default == inspect.Parameter.empty:
+        if param.kind == inspect.Parameter.KEYWORD_ONLY and \
+            param.default == inspect.Parameter.empty:
             args.append(name)
-    return tupel(args)
+    return tuple(args)
 
 def get_named_kw_args(fn):
     args = []
@@ -59,7 +59,7 @@ def get_named_kw_args(fn):
 def has_named_kw_args(fn):
     params = inspect.signature(fn).parameters
     for name, param in params.items():
-        if param.kind == inspect.Parameter.KEYWORD_ONLY
+        if param.kind == inspect.Parameter.KEYWORD_ONLY:
             return True
 
 def has_var_kw_arg(fn):
@@ -97,7 +97,7 @@ class RequestHandler(object):
 
     async def __call__(self, request):
         kw = None
-        if self._has_var_kw_arg or self._has_named_kw_args or selfl._required_kw_args:
+        if self._has_var_kw_arg or self._has_named_kw_args or self._required_kw_args:
             if request.method == 'POST':
                 if not request.content_type:
                     return web.HTTPBadRequest('missing content-type')
@@ -146,4 +146,37 @@ class RequestHandler(object):
             return r
         except APIError as e:
             return dict(error=e.error, data=e.data, message=e.message)
-            
+
+def add_static(app):
+    path = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'static')
+    app.router.add_static('/static/', path)
+    logging.info('add static {}=>{}'.format('/static/', path))
+
+def add_route(app, fn):
+    method = getattr(fn, '__method__', None)
+    path = getattr(fn, '__route__', None)
+    if path is None or method is None:
+        raise ValueError('@get or @post not defined in {}'.format(fn))
+    if not asyncio.iscoroutinefunction(fn) and not inspect.isgeneratorfunction(fn):
+        fn = asyncio.coroutine(fn)
+    logging.info('add route {} {}=>{}({})'.format(method, path, fn.__name__, 
+        ', '.join(inspect.signature(fn).parameters.keys()))
+    )
+    app.router.add_route(method, path, RequestHandler(app, fn))
+
+def add_routes(app, module_name):
+    n = module_name.rfind('.')
+    if n == -1:
+        mod = __import__(module_name, globals(), locals())
+    else:
+        name = module_name[n+1:]
+        mod = getattr(__import__(module_name[:n], globals(), locals()))
+    for attr in dir(mod):
+        if attr.startswith('_'):
+            continue
+        fn = getattr(mod, attr)
+        if callable(fn):
+            method = getattr(fn, '__medthod__', None)
+            path = getattr(fn, '__path__', None)
+            if method and path:
+                add_route(app, fn)
