46621da3c0774ea5f019302490c9865db0065119
wbjxxzx <wbjxxzx@163.com>
2018-08-30 19:29:08 +0800

    add handlers and template test.html

---
+++ www/handlers.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+# -*- coding:utf-8 -*-
+'''
+__author__ = wbjxxzx
+'''
+
+import re
+import time
+import json
+import hashlib
+import base64
+import asyncio
+from webframe import get, post
+from models import User, Comment, Blog, next_id
+
+@get('/')
+async def index(request):
+    users = await User.find_all()
+    return {
+        '__template__': 'test.html',
+        'users': users
+    }
