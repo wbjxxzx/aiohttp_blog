fe16d3a2c82759d6e649947f95e8882bbef8eb6e
wbjxxzx <wbjxxzx@163.com>
2018-09-11 09:10:56 +0800

    add mylogger and logging.conf

---
+++ www/mylogger.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+# -*- coding:utf-8 -*-
+'''
+__author__ = wbjxxzx
+'''
+
+import logging, logging.config
+logging.config.fileConfig("conf/logging.conf")
+logger = logging.getLogger("")
